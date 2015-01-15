d54563698bc7ce442d518ed6568feb2fbe9b24e1
Paweł Kaczyński <milordpanda@gmail.com>
2015-01-16 00:39:44 +0100

    ToDo list in Network.h added + Excel document with file import and diagrams added + 'Normalize' attribute in network.config added

--- Network.h
+++ Network.h
@@ -1,10 +1,3 @@
-/**
-	Main project class. Aggregates all network layers, interface and statistics.
-
-    TODO
-    - Obtaining information about layers and neurons from config file.
-*/
-
 #ifndef NETWORK
 #define NETWORK
 
@@ -16,46 +9,62 @@
 #include "XMLParser.h"
 #include <iostream>
 
+/**
+    Main project class. Aggregates all network layers, interface and statistics.
+    
+    TODO
+    - Try to generate own test data and import it from file instead of main dataset.
+    - Try to normalize data in case network is causing problems.
+      Will be added in network.config file.
+    - Test, test and test with good result reporting.
+    - Create diagrams in Excel.
+    - Comment all methods and attributes yet uncommented.
+    - Delete useless fragments of code. Especially those with tag TEMP.
+      Optionally bound these fragments into __VERBOSE or __PRINT_ALL defines.
+    - Create project documentation.
+*/
 class Network {
 public:
-	Network () {};
-	~Network () {};
+    Network () {
+    };
+    ~Network () {
+    };
 
-	// Constructor with input parameters to be passed to Interface
-	Network (int argc, char* argv[]);
+    // Constructor with input parameters to be passed to Interface
+    Network (int argc, char* argv[]);
 
-	// Main class method. Includes all operations and interfaces.
-	void run ();
+    // Main class method. Includes all operations and interfaces.
+    void run ();
 
 private:
     // Load parameters from XML config file.
     void loadParameters ();
 
-	// Load data from file (if given)
-	bool acquireData ();
+    // Load data from file (if given)
+    bool acquireData ();
 
-	// Saving results to file (if needed)
-	void saveResults ();
+    // Saving results to file (if needed)
+    void saveResults ();
 
-	// Saving stats to file (if needed)
-	void saveStats ();
+    // Saving stats to file (if needed)
+    void saveStats ();
 
-	void initializeLayers (const int& inputLayer, const int& hiddenLayer, const int& outputLayer);
+    void initializeLayers (const int& inputLayer, const int& hiddenLayer, const int& outputLayer);
 
-	void initializeLayers (std::vector<int> layers);
+    void initializeLayers (std::vector<int> layers);
 
 
-	void learn (const std::vector<ProteinData *> data);
+    void learn (const std::vector<ProteinData *> data);
     void test (CrossValidator::TestResults & results, std::vector<ProteinData*> data);
 
-	// Network modules
-	Interface mInterface;
-	DataCollector mDataCollector;
-	OutputManager mOutputManager;
-	CrossValidator mCrossValidator;
+    // Network modules
+    Interface mInterface;
+    DataCollector mDataCollector;
+    OutputManager mOutputManager;
+    CrossValidator mCrossValidator;
     XMLParser mXMLParser;
-	
-	Layer::Layers mLayersTable;
+
+    Layer::Layers mLayersTable;
 
 };
 
